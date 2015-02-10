dfcd3f43ff4ef4a72834016eb9a7f859a057fb62
Jesse Himmelstein <jesse@heavenstone.net>
2015-02-10 23:22:53 +0100

    pagination working

--- src/app/search/result/searchResult.js
+++ src/app/search/result/searchResult.js
@@ -1,6 +1,14 @@
 angular.module('search.result', [])
-    .controller("SearchResultController", function ($scope, $stateParams, search) {
+    .controller("SearchResultController", function ($scope, $state, $stateParams, search) {
         $scope.search = search;
+
+
+        $scope.goToPage = function(pageNumber) {
+            var newParams = angular.extend({}, $stateParams, {
+                page: pageNumber
+            });
+            $state.go("search", newParams, { inherit: false });
+        };
 
         search.params = $stateParams;
         search.query(); 
--- src/app/search/search.js
+++ src/app/search/search.js
@@ -12,29 +12,13 @@ angular.module('search', [
         service.searchInProgress = false; // Is a search in progress?
         service.results = [];
         
-        function makeLinkMap(linkHeader) {
-            if(!linkHeader) return {};
-
-            // Expecting a header like "http://localhost:5050/v1/event/?page=1&perPage=50; rel=next, http://localhost:5050/v1/event/?page=9&perPage=50; rel=last"
-            // link.split(",")[0].split(";")[1].trim().split("=")[1]
-            var linkMap = {};
-            var links = linkHeader.split(",");
-            for(var i = 0; i < links.length; i++) {
-                var linkDesc = links[i].split(";");
-                // On the left, the part of linkDesc after rel=
-                linkMap[linkDesc[1].split("=")[1].trim()] = linkDesc[0].trim();
-            }
-            return linkMap;
-        }
-
         service.query = function () {
             if (service.isValid()) {
                 backend.search(service.params)
                     .success(function (searchResults, status, headers) {
                         service.results = searchResults;
-                        service.linkMap = makeLinkMap(headers("link"));
-                        service.pageNumber =  headers("X-Page-Number"); 
-                        service.totalPages = headers("X-Page-Count");
+                        service.pageNumber = parseInt(headers("X-Page-Number")); 
+                        service.pageCount = parseInt(headers("X-Page-Count"));
                     })
                     .error(function(data, status) { 
                         service.results = [];
