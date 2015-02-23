0d1d58b5fb172936e630d29b746a48f768ee1f9c
Jesse Himmelstein <jesse@heavenstone.net>
2015-02-23 12:08:38 +0100

    Added CSV download link

--- src/app/search/result/searchResult.js
+++ src/app/search/result/searchResult.js
@@ -1,7 +1,16 @@
 angular.module('search.result', [])
-    .controller("SearchResultController", function ($scope, $state, $stateParams, search) {
-        $scope.search = search;
+    .controller("SearchResultController", function ($scope, $state, $stateParams, search, SERVER_URL) {
+        function makeCsvDownloadUrl() {
+            var args = [];
+            for(var key in $stateParams) {
+                if(!$stateParams[key]) continue;
 
+                args.push(key + "=" + $stateParams[key]);
+            }
+            return encodeURI(SERVER_URL + $stateParams.entityType + ".csv?" + args.join("&"));
+        }
+
+        $scope.search = search;
 
         $scope.goToPage = function(pageNumber) {
             var newParams = angular.extend({}, $stateParams, {
@@ -10,6 +19,9 @@ angular.module('search.result', [])
             $state.go("search", newParams, { inherit: false });
         };
 
+        $scope.csvDownloadUrl = makeCsvDownloadUrl();
+
         search.params = $stateParams;
         search.query(); 
+
     });
